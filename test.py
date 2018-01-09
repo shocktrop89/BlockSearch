01b582e82556a4453e2fdf7d8b8df519e67f0403
matthew <neo_x2k@hotmail.com>
2018-01-08 21:22:42 -0800

    test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-from enum import Enum
-from blockchain import blockexplorer
+#from enum import Enum
+#from blockchain import blockexplorer
 
 
-latest_block = blockexplorer.get_latest_block()
-attrs = vars(latest_block)
-print ', '.join("%s: %s" % item for item in attrs.items())
+#latest_block = blockexplorer.get_latest_block()
+#attrs = vars(latest_block)
+#print ', '.join("%s: %s" % item for item in attrs.items())
+
+print("hello world")
